ec24de40824876bc9d15677ea738422abab8a643
lapwa <lapwa@THINKPAD-X1-CARBON.lan>
2021-02-17 21:22:23 -0500

    initial commit

---
+++ src/generics/Test.java
@@ -0,0 +1,9 @@
+package generics;
+
+public class Test {
+
+	public static void main(String[] args) {
+		System.out.println("Hi");
+	}
+
+}
---
+++ src/lambda/Test.java
@@ -0,0 +1,9 @@
+package lambda;
+
+public class Test {
+
+	public static void main(String[] args) {
+		System.out.println("Hi");
+	}
+
+}
